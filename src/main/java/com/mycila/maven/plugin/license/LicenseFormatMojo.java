4fb734528977cbe992ab31c13eef95fa707a232b
Mike Hummel <mike@mhus.de>
2020-05-25 07:25:26 +0200

    step1: fork 2.11 from https://github.com/mycila/license-maven-plugin/releases/tag/license-maven-plugin-2.11

---
+++ src/main/java/com/mycila/maven/plugin/license/LicenseFormatMojo.java
@@ -0,0 +1,74 @@
+/**
+ * Copyright (C) 2008 Mycila (mathieu.carbou@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.mycila.maven.plugin.license;
+
+import com.mycila.maven.plugin.license.document.Document;
+import com.mycila.maven.plugin.license.header.Header;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Mojo;
+
+import java.io.File;
+
+/**
+ * Reformat files with a missing header to add it
+ *
+ * @author Mathieu Carbou (mathieu.carbou@gmail.com)
+ */
+@Mojo(name = "format", threadSafe = true)
+public final class LicenseFormatMojo extends AbstractLicenseMojo {
+
+    @Override
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        if(!skip) {
+            getLog().info("Updating license headers...");
+        }
+
+        AbstractCallback callback = new AbstractCallback() {
+            @Override
+            public void onHeaderNotFound(Document document, Header header) {
+                document.parseHeader();
+                if (document.headerDetected()) {
+                    if (skipExistingHeaders) {
+                        debug("Keeping license header in: %s", document.getFilePath());
+                        return;
+                    }
+                    document.removeHeader();
+                }
+                info("Updating license header in: %s", document.getFilePath());
+                document.updateHeader(header);
+                if (!dryRun) {
+                    document.save();
+                } else {
+                    String name = document.getFile().getName() + ".licensed";
+                    File copy = new File(document.getFile().getParentFile(), name);
+                    info("Result saved to: %s", copy);
+                    document.saveTo(copy);
+                }
+            }
+
+            @Override
+            public void onExistingHeader(Document document, Header header) {
+                debug("Header OK in: %s", document.getFilePath());
+            }
+        };
+
+        execute(callback);
+
+        callback.checkUnknown();
+    }
+
+}
---
+++ src/main/java/com/mycila/maven/plugin/license/LicenseRemoveMojo.java
@@ -0,0 +1,75 @@
+/**
+ * Copyright (C) 2008 Mycila (mathieu.carbou@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.mycila.maven.plugin.license;
+
+import com.mycila.maven.plugin.license.document.Document;
+import com.mycila.maven.plugin.license.header.Header;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Mojo;
+
+import java.io.File;
+
+/**
+ * Remove the specified header from source files
+ *
+ * @author Mathieu Carbou (mathieu.carbou@gmail.com)
+ */
+@Mojo(name = "remove", threadSafe = true)
+public final class LicenseRemoveMojo extends AbstractLicenseMojo {
+
+    @Override
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        if(!skip) {
+            getLog().info("Removing license headers...");
+        }
+
+        AbstractCallback callback = new AbstractCallback() {
+
+            @Override
+            public void onHeaderNotFound(Document document, Header header) {
+                debug("Header was not found in: %s (But keep trying to find another header to remove)", document.getFile());
+                remove(document);
+            }
+
+            @Override
+            public void onExistingHeader(Document document, Header header) {
+                info("Removing license header from: %s", document.getFile());
+                remove(document);
+            }
+
+        };
+
+        execute(callback);
+
+        callback.checkUnknown();
+    }
+
+    private void remove(Document document) {
+        document.parseHeader();
+        if (document.headerDetected())
+            document.removeHeader();
+        if (!dryRun) {
+            document.save();
+        } else {
+            String name = document.getFile().getName() + ".licensed";
+            File copy = new File(document.getFile().getParentFile(), name);
+            info("Result saved to: %s", copy);
+            document.saveTo(copy);
+        }
+    }
+
+}
